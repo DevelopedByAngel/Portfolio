9d8884ce698049cfc67f237676f15237514e6a70
Angel Francis <50880391+DevelopedByAngel@users.noreply.github.com>
2020-06-11 13:00:56 +0530

    Add files via upload

---
+++ portfolioabout.js
@@ -0,0 +1,61 @@
+/*since ths is called after portfolio.js this code will have affect in portfoilioabout*/
+$('#menu').mouseout(function()
+	{
+		$('#underline').css('left','120px');
+		$('#underline').css('width','80px');
+	});
+$(window).ready(function()
+	{
+		// $('#skills > img').attr("onclick","next(this);");
+		var x=2;
+		var y=x-1;
+		var z=x+1;
+		$('#'+x).css("filter","blur(0px)");
+		$('#'+y+',#'+z).css('height','80px');	
+		$('#skills > img:not(#'+x+',#'+y+',#'+z+')').css("height","10px");
+		$('#skills > img:not(#'+x+',#'+y+',#'+z+')').css("display","none");
+	});
+function next()
+	{
+		var x=$('#skills')[0];
+		$('#skills > img').css("display","inline");
+		$('#skills > img').css("height","100px");
+		$('#skills > img').css("filter","blur(5px)");
+		var x=parseInt(x.className);
+		x=x+1;
+		var y=x-1;
+		var z=x+1;
+		if(x==12){
+			x=1
+			y=2
+			z=3
+		}
+		$('#skills').attr('class',x);
+		$('#'+x).css("filter","blur(0px)");
+		$('#'+y+',#'+z).css('height','80px');	
+		$('#skills > img:not(#'+x+',#'+y+',#'+z+')').css("height","10px");
+		$('#skills > img:not(#'+x+',#'+y+',#'+z+')').css("display","none");
+
+	};
+function previous()
+	{
+		var x=$('#skills')[0];
+		$('#skills > img').css("display","inline");
+		$('#skills > img').css("height","100px");
+		$('#skills > img').css("filter","blur(5px)");
+		var x=parseInt(x.className);
+		x=x-1;
+		var y=x-1;
+		var z=x+1;
+		if(x==0){
+			x=11
+			y=10
+			z=9
+		}
+		$('#skills').attr('class',x);
+		$('#'+x).css("filter","blur(0px)");
+		$('#'+y+',#'+z).css('height','80px');	
+		$('#skills > img:not(#'+x+',#'+y+',#'+z+')').css("height","10px");
+		$('#skills > img:not(#'+x+',#'+y+',#'+z+')').css("display","none");
+
+	};
---
+++ portfolioworks.js
@@ -0,0 +1,5 @@
+$('#menu').mouseout(function()
+	{
+		$('#underline').css('left','227px');
+		$('#underline').css('width','80px');
+	});
