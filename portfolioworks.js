c36f40325840c170af591098046c343af7b96f3f
Angel Francis <50880391+DevelopedByAngel@users.noreply.github.com>
2020-07-12 16:09:33 +0530

    Update portfolioworks.js

--- portfolioworks.js
+++ portfolioworks.js
@@ -3,3 +3,34 @@ $('#menu').mouseout(function()
 		$('#underline').css('left','227px');
 		$('#underline').css('width','80px');
 	});
+const small=$('.container')[0];
+const bootstrap=$('#bootstrap')[0];
+const big=$(".bigscreen")[0];
+$(document).ready(function()
+	{
+if($(window).width()<1000 || $(window).height()<400)
+			{
+				// $('#bootstrap').remove();
+				$('.bigscreen').remove();
+			}
+		else
+		{
+			$('#bootstrap').remove();
+			$('.container').remove();
+		}
+	});
+$(window).resize(function()
+	{
+		if($(window).width()<1000 || $(window).height()<400)
+			{
+				$('head > link').before(bootstrap);
+				$(".bigscreen").before(small);
+				$('.bigscreen').remove();
+			}
+		else
+		{
+			$('#bootstrap').remove();
+			$(".container").before(big);
+			$('.container').remove();
+		}
+	});
